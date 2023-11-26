aeafd8c5852526c04e5fab99057ba572b8c0a5cf
Sunith Acharya <sunithacharya312@gmail.com>
2023-11-26 20:01:56 +0530

    Initial commit, added fonts and created file structure format

---
+++ SpatiumVisitors/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import SignIn from './src/screens/sign_in';
+
+AppRegistry.registerComponent(appName, () => SignIn);
